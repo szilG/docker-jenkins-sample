9874a262fcca441223338329d77443bc36de5223
szilG <gombiszilvi@gmail.com>
2022-08-24 09:54:50 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from inspect import ismethoddescriptor
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "alexa" in command:
+                command = command.replace("alexa", "")
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    if "play" in command:
+        song = command.replace("play", "")
+        talk("playing" + song)
+        pywhatkit.playonyt(song)
+    elif "time" in command:
+        time = datetime.datetime.now().strftime("%I:%M %p")
+        talk("Current time is " + time)
+    elif "search" in command:
+        person = command.replace("search", "")
+        info = wikipedia.summary(person, 2)
+        talk(info)
+    elif "are you single" in command:
+        talk("I am in a relationship with wifi")
+    elif "joke" in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk("Please say the command again.")
+
+
+while True:
+    run_alexa()
